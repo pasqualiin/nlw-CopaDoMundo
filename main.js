0b709a662e1bb8172ad77aea51a832862b7a7b58
Guilherme Pasqualin <guilhermepasqualin@gmail.com>
2022-11-04 18:26:06 -0300

    adding responsive layout

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ function createGame(player1, hour, player2) {
   </li>`
 }
 
+let delay = -0.3
 function createCard(date, day, games) {
-  return `<div class="card">
+  delay += 0.3
+  return `<div class="card" style="animation-delay: ${delay}s">
           <h2> ${date} <span>${day}</span></h2>
           <ul>
             ${games}
@@ -15,23 +17,13 @@ function createCard(date, day, games) {
         </div>`
 }
 
-document.querySelector("#app").innerHTML = `
-  <header>
-    <img src="./assets/logo.svg" alt="Logo da NLW" />
-  </header>
-      <main id="cards">
-        ${createCard(
-          "24/11",
-          "Quinta-feira",
-          createGame("switzerland", "07:00", "cameroon") +
-            createGame("uruguay", "10:00", "southKorea") +
-            createGame("portugal", "13:00", "ghana") +
-            createGame("brazil", "16:00", "serbia")
-        )}
-        ${createCard(
-          "28/11",
-          "Segunda-feira",
-          createGame("brazil", "16:00", "serbia")
-        )}
-      </main>
-      `
+document.querySelector("#cards").innerHTML =
+  createCard(
+    "24/11",
+    "Quinta-feira",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("uruguay", "10:00", "southKorea") +
+      createGame("portugal", "13:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
+  createCard("28/11", "Segunda-feira", createGame("brazil", "16:00", "serbia"))
